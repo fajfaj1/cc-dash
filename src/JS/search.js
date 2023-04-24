58f745f85b08cda3652dc593862fdc423898a2f2
fajfaj1 <97434565+fajfaj1@users.noreply.github.com>
2023-04-24 22:36:18 +0200

    1.5 - fix display in dark theme
    + add searchbar placeholder

--- src/JS/search.js
+++ src/JS/search.js
@@ -14,7 +14,7 @@ getConfig().then(config => {
 
 function initiate() {
 
-    const searchIcon = `<svg xmlns="http://www.w3.org/2000/svg" stroke="currentColor" height="1em" width="1em" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>`
+    const searchIcon = `<svg xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="currentColor" height="1em" width="1em" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>`
 
     const cmd = {
         toJSON: (element) => {
@@ -84,7 +84,7 @@ function initiate() {
     searchbox.setAttribute('class', 'searchbox-wrapper')
     searchbox.setAttribute('for', 'searchbox')
     searchbox.innerHTML = `
-        <input type="search" id="searchbox">
+        <input type="search" id="searchbox" placeholder="Search query...">
         <button class="searchIcon" id="searchSubmit">
             ${searchIcon}
         </button>
